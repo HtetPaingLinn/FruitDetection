0f6ebba98080c7820111192c71a769a5159d878f
HtetPaingLinn <htetpainglinn2822004@gmail.com>
2024-07-21 23:48:15 +0630

    First Commit

--- app2.py
+++ app2.py
@@ -7,8 +7,8 @@ import urllib.request
 
 def main():
     st.title("Fruit Tracking and Inspection")
-    # Open the laptop camera
-    cap = cv2.VideoCapture(0)
+
+    # Define a function to get the model directory based on the selected fruit
     def get_model_directory(selected_fruit):
         fruit_model_mapping = {
             "Tomato": "best1.pt",
@@ -28,32 +28,42 @@ def main():
 
     # Display information about the selected fruit
     st.sidebar.info(f"Selected Fruit: {selected_fruit}")
-    st.sidebar.info("This app uses YOLOv8 to track and inspects fruits in real-time.")
+    st.sidebar.info("This app uses YOLOv8 to track and inspect fruits in real-time.")
 
     # Placeholder for video display
     video_placeholder = st.empty()
-    if st.button("Start Tracking"):
+    
+    tracking = st.button("Start Tracking")
+    stop_tracking = st.button("Stop Tracking")
+
+    if tracking:
+        # Open the laptop camera
+        cap = cv2.VideoCapture(0)
+
         # Loop through the camera frames
-        while True:
-            try:
-                # Read a frame from the camera
-                success, frame = cap.read()
-
-                # Run YOLOv8 tracking on the frame, persisting tracks between frames
-                results = model.track(frame, persist=True)
-                annotated_frame = results[0].plot()
-                annotated_array = np.array(annotated_frame)
-
-                # Display the annotated frame in the main area
-                video_placeholder.image(annotated_array, channels="BGR")
-            except:
+        while tracking:
+            # Read a frame from the camera
+            success, frame = cap.read()
+            if not success:
                 st.warning("Unable to capture video. Please check your camera.")
+                break
+
+            # Run YOLOv8 tracking on the frame, persisting tracks between frames
+            results = model.track(frame, persist=True)
+            annotated_frame = results[0].plot()
+            annotated_array = np.array(annotated_frame)
+
+            # Display the annotated frame in the main area
+            video_placeholder.image(annotated_array, channels="BGR")
+
+            # Check if the stop button was pressed
+            if stop_tracking:
                 break
 
         # Release the video capture object
         cap.release()
-    elif st.button("Stop Tracking"):
-        video_placeholder.info("Tracking Disabled")
+        st.success("Tracking stopped")
 
 if __name__ == "__main__":
     main()
+
